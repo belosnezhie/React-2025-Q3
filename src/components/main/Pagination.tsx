011d2417b1fc72f0efe3222d83e1e367e5831747
Maria Treier <mariatraier@gmail.com>
2024-07-10 12:49:21 +0200

    feat: implement pagination logic

---
+++ src/components/main/Pagination.tsx
@@ -0,0 +1,31 @@
+interface PaginationProps {
+  currentPage: number;
+  pagesUmmount: number;
+  updatePageCallback: (pageNumber: number) => void;
+}
+
+const Pagination = ({
+  updatePageCallback,
+  pagesUmmount,
+  currentPage,
+}: PaginationProps) => {
+  return (
+    <>
+      <div className="pagination">
+        {Array.from({ length: pagesUmmount }, (item, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              updatePageCallback(index + 1);
+            }}
+            className={`pagination-button ${currentPage === index + 1 ? 'active' : ''}`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 
 import Header from '../../components/header/Header.tsx';
 import CardsWrapper from '../../components/main/CardsWrapper.tsx';
+import Pagination from '../../components/main/Pagination.tsx';
 import { PeopleSearchResp, SearchResp } from '../../model/TypesStarWars';
 import { ApiService, apiService } from '../../services/ApiService';
 import { searchQueryStorage } from '../../services/LocalStorage';
@@ -30,6 +31,21 @@ const MainPage = () => {
       return res;
     },
     [service, storage],
+  );
+
+  const handlePageChange = useCallback(
+    async (pageNumber: number): Promise<SearchResp> => {
+      setLoading(true);
+
+      const res: SearchResp = await service.getDefaultData(pageNumber);
+
+      setCharactersData(res.results);
+
+      setLoading(false);
+
+      return res;
+    },
+    [service],
   );
 
   const getDefaultData = useCallback(async () => {
@@ -67,6 +83,11 @@ const MainPage = () => {
         ) : (
           <CardsWrapper cardCharactersData={charactersData} />
         )}
+        <Pagination
+          updatePageCallback={handlePageChange}
+          currentPage={0}
+          pagesUmmount={9}
+        />
         <div className="yoda" />
       </main>
     </>
