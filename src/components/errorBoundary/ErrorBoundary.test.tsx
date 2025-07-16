8a418f56ce515036b906729538ad1ffe5060819c
Maria Treier <mariatraier@gmail.com>
2025-07-16 19:22:04 +0200

    feat: set up test environment, implement error boundary test

---
+++ src/components/errorBoundary/ErrorBoundary.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { useState } from 'react';
+import { expect, test } from 'vitest';
+
+import ErrorBoundary from './ErrorBoundary.tsx';
+
+test('should show fallback UI when error', () => {
+  const Child = () => {
+    const [wasClicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+      setClicked(true);
+    };
+
+    if (wasClicked) {
+      throw new Error('Test error');
+    }
+
+    return (
+      <button
+        className="throw_error_button"
+        type="button"
+        onClick={() => {
+          handleClick();
+        }}
+        data-testid="error_button"
+      >
+        Generate error
+      </button>
+    );
+  };
+
+  render(
+    <ErrorBoundary>
+      <Child />
+    </ErrorBoundary>,
+  );
+
+  const errorButton = screen.getByTestId('error_button');
+
+  fireEvent.click(errorButton);
+
+  const fallbackPage = screen.getByText(
+    'Oops! Something went wrong. Please try again later.',
+  );
+
+  expect(fallbackPage).toBeDefined();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '',
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/main.tsx',
+        'src/App.tsx',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  },
 });
