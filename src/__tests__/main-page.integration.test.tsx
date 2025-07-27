fe7dbe7d4b4738867eb38ba7994628bf76ccf81a
Maria Treier <mariatraier@gmail.com>
2025-07-27 12:30:57 +0200

    Fix

--- src/__tests__/main-page.integration.test.tsx
+++ src/__tests__/main-page.integration.test.tsx
@@ -1,42 +1,67 @@
 import { render, screen, waitFor } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
+import { describe, expect, it, MockedFunction, vi } from 'vitest';
 
+import * as hooks from '@/hooks/use-local-storage';
 import { DefaultSearchResp } from '@/model/types-star-wars';
 import { ApiService } from '@/services/api-service';
-import { searchQueryStorage } from '@/services/local-storage';
 
 import { MainPage } from '../pages/main/main-page';
 
 describe('Integration Tests', () => {
-  it.skip('makes initial API call on component mount', async () => {
+  beforeAll(() => {
+    vi.mock('@/hooks/use-local-storage');
+  });
+  let mock: MockedFunction<
+    (initialValue: string) => [string, (nextState: string) => void]
+  >;
+  beforeEach(() => {
+    mock = vi.mocked(hooks.useLocalStorage).mockReturnValue([
+      'Jane Doe',
+      (_: string): void => {
+        // do nothing
+      },
+    ]);
+  });
+  afterEach(() => {
+    mock.mockReset();
+  });
+
+  it('makes initial API call on component mount', async () => {
     const mockService: ApiService = {
       getDefaultData: vi.fn().mockResolvedValue(new DefaultSearchResp()),
       getSeachedData: vi.fn(),
     } satisfies ApiService;
 
-    render(<MainPage service={mockService} />);
-
-    await waitFor(() => {
-      expect(mockService.getDefaultData).toHaveBeenCalled();
-    });
-  });
-
-  it.skip('handles search term from localStorage on initial load', async () => {
-    const savedSearchQuery = 'Jane Doe';
-
-    vi.spyOn(searchQueryStorage, 'getSearchQuery').mockReturnValue(
-      savedSearchQuery,
+    render(
+      <BrowserRouter>
+        <MainPage service={mockService} />
+      </BrowserRouter>,
     );
 
-    render(<MainPage service={new ApiService()} />);
-
     await waitFor(() => {
-      expect(searchQueryStorage.getSearchQuery).toHaveBeenCalled();
+      expect(mock).toHaveBeenCalled();
     });
   });
 
-  it.skip('manages loading states during API calls', async () => {
-    render(<MainPage service={new ApiService()} />);
+  it('handles search term from localStorage on initial load', async () => {
+    render(
+      <BrowserRouter>
+        <MainPage service={new ApiService()} />
+      </BrowserRouter>,
+    );
+
+    await waitFor(() => {
+      expect(mock).toHaveBeenCalled();
+    });
+  });
+
+  it('manages loading states during API calls', async () => {
+    render(
+      <BrowserRouter>
+        <MainPage service={new ApiService()} />
+      </BrowserRouter>,
+    );
 
     const spinner = screen.findByRole('spinner');
 
